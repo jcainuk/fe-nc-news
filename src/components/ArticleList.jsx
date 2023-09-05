927f1ad0f5436c694db0862743b8d6e7db21b59b
Jonathan Cain <jcainuk@gmail.com>
2023-09-05 10:57:28 +0100

    refactors design for displaying articles from tabular data to news items cards instead to reflect the nature of this project

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -20,36 +20,34 @@ const ArticleList = (props) => {
   }, []);
 
   return (
-    <div className="list-group">
-      <table className="table table-hover table-light">
-        <thead>
-          <tr className="bg-primary">
-            <th scope="col">Title</th>
-            <th scope="col">Topic</th>
-            <th scope="col">Author</th>
-            <th scope="col">Date Created</th>
-            <th scope="col">Votes</th>
-            <th scope="col">Click to View</th>
-          </tr>
-        </thead>
-        <tbody>
-          {articles &&
-            articles.map((article) => {
-              return (
-                <tr key={`${article.title}+${article.index}`}>
-                  <td>{article.title}</td>
-                  <td>{article.topic}</td>
-                  <td>{article.author}</td>
-                  <td>{convertDate(article.created_at)}</td>
-                  <td>{article.votes}</td>
-                  <td>
-                    <button className="btn btn-warning">View article</button>
-                  </td>
-                </tr>
-              );
-            })}
-        </tbody>
-      </table>
+    <div className="row">
+      {articles &&
+        articles.map((article) => {
+          return (
+            <div key={article.article_id} className="col-md-4 mb-4">
+              <div className="card d-flex flex-column h-100">
+                <img
+                  src={article.article_img_url}
+                  className="card-img-top"
+                  alt={article.title}
+                />
+                <div className="card-body text-center d-flex flex-column">
+                  <h5 className="card-title">{article.title}</h5>
+                  <p className="card-text">Topic: {article.topic}</p>
+                  <p className="card-text">Author: {article.author}</p>
+                  <p className="card-text">
+                    Date Created: {convertDate(article.created_at)}
+                  </p>
+                  <p className="card-text">Comments: {article.comment_count}</p>
+                  <p className="card-text">Votes: {article.votes}</p>
+                  <button className="btn btn-warning mt-auto">
+                    View article
+                  </button>
+                </div>
+              </div>
+            </div>
+          );
+        })}
     </div>
   );
 };
