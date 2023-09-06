8fddaade9f13c2c971afd7046237f3f84d16d7b7
Jonathan Cain <jcainuk@gmail.com>
2023-09-06 11:46:15 +0100

    completes refactor or error codes in articledetailpage

--- src/routes/ArticleDetailPage.jsx
+++ src/routes/ArticleDetailPage.jsx
@@ -9,24 +9,15 @@ const ArticleDetailPage = () => {
   const [selectedArticle, setSelectedArticle] = useState(null);
   const [comments, setComments] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const articleResponsePromise = ArticleFinder.get(`/articles/${id}`);
-        const commentsResponsePromise = ArticleFinder.get(
+        const articleResponse = await ArticleFinder.get(`/articles/${id}`);
+        const commentsResponse = await ArticleFinder.get(
           `/articles/${id}/comments`
-        ).catch((error) => {
-          if (error.response && error.response.status === 404) {
-            return { data: { comments: [] } };
-          }
-          throw error;
-        });
-
-        const [articleResponse, commentsResponse] = await Promise.all([
-          articleResponsePromise,
-          commentsResponsePromise
-        ]);
+        );
 
         setSelectedArticle(articleResponse.data.article);
         setComments(commentsResponse.data.comments);
@@ -34,6 +25,7 @@ const ArticleDetailPage = () => {
       } catch (err) {
         console.error(err);
         setIsLoading(false);
+        setIsError(true);
       }
     };
     fetchData();
@@ -45,37 +37,43 @@ const ArticleDetailPage = () => {
       <div className="container mt-4">
         {isLoading ? (
           <p>Loading...</p>
-        ) : selectedArticle ? (
+        ) : isError ? (
+          <p>An error occurred while fetching data.</p>
+        ) : (
           <div className="row justify-content-center">
             <div className="col-12 col-md-8">
-              <div className="card">
-                <img
-                  src={selectedArticle.article_img_url}
-                  className="card-img-top"
-                  alt={selectedArticle.title}
-                />
-                <div className="card-body">
-                  <h5 className="card-title">{selectedArticle.title}</h5>
-                  <p className="card-text">Topic: {selectedArticle.topic}</p>
-                  <p className="card-text">Author: {selectedArticle.author}</p>
-                  <p className="card-text">{selectedArticle.body}</p>
-                  <p className="card-text">
-                    Date Created:{" "}
-                    {new Date(selectedArticle.created_at).toLocaleDateString()}
-                  </p>
-                  <p className="card-text">Votes: {selectedArticle.votes}</p>
+              {selectedArticle && (
+                <div className="card">
+                  <img
+                    src={selectedArticle.article_img_url}
+                    className="card-img-top"
+                    alt={selectedArticle.title}
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{selectedArticle.title}</h5>
+                    <p className="card-text">Topic: {selectedArticle.topic}</p>
+                    <p className="card-text">
+                      Author: {selectedArticle.author}
+                    </p>
+                    <p className="card-text">{selectedArticle.body}</p>
+                    <p className="card-text">
+                      Date Created:{" "}
+                      {new Date(
+                        selectedArticle.created_at
+                      ).toLocaleDateString()}
+                    </p>
+                    <p className="card-text">Votes: {selectedArticle.votes}</p>
+                  </div>
                 </div>
+              )}
+              <div className="mt-4">
+                {comments.length === 0 ? (
+                  <p>No comments yet.</p>
+                ) : (
+                  <CommentList comments={comments} />
+                )}
               </div>
             </div>
-          </div>
-        ) : (
-          <p>No article found.</p>
-        )}
-        {comments.length === 0 ? (
-          <div className="mt-4">No comments yet.</div>
-        ) : (
-          <div>
-            <CommentList comments={comments} />
           </div>
         )}
       </div>
